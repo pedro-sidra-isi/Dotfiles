b7e716db015a73607b472b33522d4fff2febe09d
Pedro Freitas <pedrosidra0@gmail.com>
2018-10-18 15:23:11 -0300

    Comment install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# TODO: install stuff :(
+# Updates plugins that are added as submodules
 git submodule update --init --recursive
 
-# Install dependencies:
+# Install stuff:
 sudo apt install vim
 sudo apt install build-essential
-sudo apt install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt install cmake git libgtk2.0-dev pkg-config
 
-# Install vundle and rice vim
+# Install vundle (Plugin manager for vim)
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# If there's not a line in .vimrc sourcing my rc, source it
 grep -q -F 'source ~/Dotfiles/vim/vimrc.vim' ~/.vimrc|| echo 'source ~/Dotfiles/vim/vimrc.vim' >> ~/.vimrc 
+# Install plugins
 vim +PluginInstall +qall
 
-# Install Tmuxline (should be inside tmux)
+# Install Tmuxline (should be inside tmux, or this fails)
 vim -c 'Tmuxline lightline' +qall
 vim -c 'TmuxlineSnapshot ~/Dotfiles/tmux/snapshot' +qall
 
@@ -36,8 +38,9 @@ fi
 
 echo 'source-file ~/Dotfiles/tmux/tmux.conf' > ~/.tmux.conf
 
-# Install oh-my-zsh
+# Install oh-my-zsh manually
 sudo apt-get install zsh
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+# change default shell to zsh
 chsh -s /bin/zsh
 echo 'source ~/Dotfiles/zsh/zshrc.sh' > ~/.zshrc
