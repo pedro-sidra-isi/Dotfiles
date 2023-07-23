022e0b5d72a4fd9096372e6bf1bf8162933580da
Pedro Sidra de Freitas <pedro.sidra@senairs.org.br>
2023-07-23 17:51:46 -0300

    2023 update

--- install.sh
+++ install.sh
@@ -1,50 +1,17 @@
 #!/bin/bash
 
-# Updates plugins that are added as submodules
-git submodule update --init --recursive
-
 # Install stuff:
-sudo apt install vim
 sudo apt install build-essential
 sudo apt install cmake git libgtk2.0-dev pkg-config
-
-# Install vundle (Plugin manager for vim)
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# If there's not a line in .vimrc sourcing my rc, source it
-grep -q -F 'source ~/Dotfiles/vim/vimrc.vim' ~/.vimrc|| echo 'source ~/Dotfiles/vim/vimrc.vim' >> ~/.vimrc 
-# Install plugins
-vim +PluginInstall +qall
-
-# Install Tmuxline (should be inside tmux, or this fails)
-vim -c 'Tmuxline lightline' +qall
-vim -c 'TmuxlineSnapshot ~/Dotfiles/tmux/snapshot' +qall
-
-# Install YouCompleteMe
-sudo apt-get install python3-dev
-if ! ls ~/.vim/bundle/YouCompleteMe/python
-then
-	python3 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
-fi
-
-# Install powerline fonts (set in terminal for prettiness)
-if ! ( ls ~/.local/share/fonts/ | grep powerline )
-then
-	git clone https://github.com/powerline/fonts.git --depth=1
-	cd fonts
-	./install.sh
-	cd ..
-	rm -rf fonts
-fi
 
 echo 'source-file ~/Dotfiles/tmux/tmux.conf' > ~/.tmux.conf
 
 # Install oh-my-zsh manually
 sudo apt-get install zsh
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 # change default shell to zsh
 chsh -s /bin/zsh
 echo 'source ~/Dotfiles/zsh/zshrc.sh' > ~/.zshrc
-
-sudo apt-get install python3-pip
-pip3 install --user pynvim
-
--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -6,13 +6,8 @@ export ZSH=$HOME/.oh-my-zsh
 # Theme
 ZSH_THEME="agnoster"
 
-# Settings
-export PLUGINS=$HOME/Dotfiles/zsh/plugins
-
 PATH="$HOME/.local/bin:$PATH"
 
-
-#Funcoes
 
 # CD com ls
 c(){
@@ -27,24 +22,19 @@ pInit(){
     cp $PYTHON_SKEL_PATH ./$1;
 }
 
-
-# Ultimo diretorio apenas 
-# (porra, usa pwd pra mostrar tudo, n precisa sempre)
-
 # Plugins
 
 plugins=(
-git
-vi-mode
+	git
+	vi-mode
+	zsh-autosuggestions
+	zsh-syntax-highlighting
 )
 source $ZSH/oh-my-zsh.sh
 
 	prompt_dir() {
   prompt_segment blue black '%c'
 }
-source $PLUGINS/zsh-autosuggestions/zsh-autosuggestions.zsh
-source $PLUGINS/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
 
 # Disable ctrl-s shortcut that freezes terminal
 stty -ixon
